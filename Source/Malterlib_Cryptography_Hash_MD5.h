7c702521f837fa257796461d3c20c3270b995d76
Erik Olofsson <erik@olofsson.info>
2019-05-21 10:37:41 +0200

    [Malterlib/Cryptography] Move hash cache to it's own files
    Refactor registry

--- Source/Malterlib_Cryptography_Hash_MD5.h
+++ Source/Malterlib_Cryptography_Hash_MD5.h
@@ -15,25 +15,6 @@ namespace NMib::NCryptography
 	extern template struct TCHashImpl_BoringSSL<EDigestType_MD5, 16>;
 	extern template struct TCHashImpl<TCHashImpl_BoringSSL<EDigestType_MD5, 16>>;
 	extern template struct TCMessageDigest<CHash_MD5::mc_DigestSize, CHash_MD5>;
-
-	struct CHashCache
-	{
-		CHashCache(NStr::CStr const &_File, bool _bUpdateHash, bool _bAlwaysCheckHash);
-		~CHashCache();
-
-		bool f_SetAlwaysCheckHash(bool _bAlwaysCheck);
-
-		void f_SaveToFile(NStr::CStr const &_File);
-		CHashDigest_MD5 f_GetHash(NStr::CStr const &_FileName, NStr::CStr const &_AlternateFileName = NStr::CStr());
-		void f_SetHash(NStr::CStr const &_FileName, CHashDigest_MD5 const &_Digest);
-
-	private:
-		NStr::CStr mp_HashFileName;
-		bool mp_bUpdateHash;
-		bool mp_bAlwaysCheckHash;
-		NContainer::TCMap<NStr::CStr, CHashDigest_MD5> mp_CachedDigests;
-		NThread::CMutual mp_Lock;
-	};
 }
 
 #ifndef DMibPNoShortCuts
---
+++ Source/Malterlib_Cryptography_Hash_MD5_Cache.h
@@ -0,0 +1,32 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#pragma once
+
+#include "Malterlib_Cryptography_Hash_MD5.h"
+
+namespace NMib::NCryptography
+{
+	struct CHashCache
+	{
+		CHashCache(NStr::CStr const &_File, bool _bUpdateHash, bool _bAlwaysCheckHash);
+		~CHashCache();
+
+		bool f_SetAlwaysCheckHash(bool _bAlwaysCheck);
+
+		void f_SaveToFile(NStr::CStr const &_File);
+		CHashDigest_MD5 f_GetHash(NStr::CStr const &_FileName, NStr::CStr const &_AlternateFileName = NStr::CStr());
+		void f_SetHash(NStr::CStr const &_FileName, CHashDigest_MD5 const &_Digest);
+
+	private:
+		NStr::CStr mp_HashFileName;
+		bool mp_bUpdateHash;
+		bool mp_bAlwaysCheckHash;
+		NContainer::TCMap<NStr::CStr, CHashDigest_MD5> mp_CachedDigests;
+		NThread::CMutual mp_Lock;
+	};
+}
+
+#ifndef DMibPNoShortCuts
+	using namespace NMib::NCryptography;
+#endif
